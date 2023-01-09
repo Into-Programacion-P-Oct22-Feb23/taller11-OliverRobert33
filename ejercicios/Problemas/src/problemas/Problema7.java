2b3df98ea468e3d9a54fa6fd1408b88a4375d4b3
Oliver Saraguro <oliversaraguro2004>
2023-01-09 16:20:52 -0500

    Taller 11

--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -7,16 +7,21 @@ package problemas;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 /**
  *
  * @author OLIVER SARAGURO - RUBEN CONDOY
  */
 public class Problema7 {
 
+    static String[] arreglo1;
+
     public static void main(String[] args) {
         String salida;
         salida = funcionElementos();
-        System.out.printf("Las ciudades del ecuador son:\n%s\n\n", salida);
+        System.out.printf("\nLas ciudades del ecuador son:\n%s\n",
+                salida);
 
         procedimientoElementos();
 
@@ -24,16 +29,18 @@ public class Problema7 {
 
     public static String funcionElementos() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese cuanto elementos nescecita en el Arreglo 1: ");
+        System.out.println("Ingrese cuantos elementos para llenar el Arreglo : ");
 
         int i = entrada.nextInt();
         String salida = "";
-        String[] arreglo1 = new String[i];
+        arreglo1 = new String[i];
         entrada.nextLine();
-        
+
         for (int f = 0; f < arreglo1.length; f++) {
+            String ciudad;
             System.out.println("Ingrese Ciudades del Ecuador: ");
-            String ciudad = entrada.nextLine();
+            ciudad = entrada.nextLine();
+
             arreglo1[f] = ciudad;
         }
 
@@ -44,26 +51,14 @@ public class Problema7 {
     }
 
     public static void procedimientoElementos() {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese cuanto elementos nescecita en el Arreglo 2: ");
-        int i = entrada.nextInt();
 
         String salida = "";
-        String[] arreglo2 = new String[i];
-        entrada.nextLine();
-        
-        for (int j = 0; j < arreglo2.length; j++) {
-            System.out.println("Ingrese elementos de tipo cadena para el "
-                    + "Arreglo 2: ");
-            arreglo2[j] = entrada.nextLine();
-        }
-        
-        for (int j = 0; j < arreglo2.length; j++) {
-            if (arreglo2[j].length() >= 4 && arreglo2[j].length() <= 5) {
-                salida = String.format("%s%s ", salida, arreglo2[j]);    
+        for (int j = 0; j < arreglo1.length; j++) {
+            if (arreglo1[j].length() >= 4 && arreglo1[j].length() <= 5) {
+                salida = String.format("%s%s ", salida, arreglo1[j]);
             }
         }
-        System.out.printf("Los elemenos del Arreglo 2; de 4 y 5 caracteres "
-                        + "son: \n%s\n", salida);
+        System.out.printf("Los elemenos del Arreglo de 4 y 5 caracteres "
+                + "son: \n%s\n", salida);
     }
 }
